02d8f097f694eb67800d8024e9f66ebe05c9c14b
shuaijiumei <740514874@qq.com>
2021-04-22 18:22:34 +0800

    AJAX

--- learnAJAX/server/app.js
+++ learnAJAX/server/app.js
@@ -14,15 +14,37 @@ app.get('/server', (request, response) => {
 
 });
 //接受任意请求
-app.all('/server', (request, response) => {
+app.all('/json-server', (request, response) => {
     // 设置响应头 允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
+    //响应一个数据
+    const data = {
+        name: 'tby',
+        lover: 'hx'
+    };
+    let str = JSON.stringify(data);
     //设置响应体
-    response.send('Hello,AJAX--POST');
-
+    response.send(str);
 
 });
+app.all('/axios-server', (request, response) => {
+    // 设置响应头 允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    //响应一个数据
+    const data = {
+        name: 'tby',
+        lover: 'hx'
+    };
+    let str = JSON.stringify(data);
+    //设置响应体
+    response.send(str);
+
+});
+
+
+
 
 app.listen(8000, () => {
     console.log("服务已经启动，8000端口监听中...");
